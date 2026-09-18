330297d3e6ad4947c689e0ac92812c5a4d7a247a
Kaung Htet <kh622962@gmail.com>
2026-09-18 08:45:04 +0630

    refactor l12emailbox

--- l12emailbox/app.js
+++ l12emailbox/app.js
@@ -1,75 +1,75 @@
 // GET UI
 
-const getemail = document.getElementById("emails");
-const gettextarea = document.querySelector("textarea");
-const getbtn = document.querySelector(".btn");
-const getemcontainer = document.querySelector(".email-container");
+const EMAIL_INPUT_BOX = document.getElementById("emails");
+const TEXTAREA = document.querySelector("textarea");
+const SEND_BTN = document.querySelector(".btn");
+const EMAIL_CONTAINER = document.querySelector(".email-container");
 
-getemail.focus();
+EMAIL_INPUT_BOX.focus();
 
-getemail.addEventListener("keyup", function () {
+EMAIL_INPUT_BOX.addEventListener("keyup", function () {
   // console.log(e.target.value);
   // console.log(this.value);
 
-  createemaillist(this.value);
+  createEmailList(this.value);
 });
 
-function createemaillist(inputtext) {
-  // console.log(inputtext);
+function createEmailList(inputText) {
+  // console.log(inputText);
 
   // split by (',')
-  // const eitems = inputtext.split(",");
-  // console.log(eitems);
+  // const EMAIL_TEXT = inputText.split(",");
+  // console.log(EMAIL_TEXT);
 
   // remove empty, space
-  // const eitems = inputtext
+  // const EMAIL_TEXT = inputText
   //   .split(",")
   //   .filter((rmempty) => rmempty.trim() !== "");
-  // console.log(eitems);
+  // console.log(EMAIL_TEXT);
 
   // remove space in text
-  const eitems = inputtext
-    .split(",")
-    .filter((rmempty) => rmempty.trim() !== "")
-    .map((rmtxtempty) => rmtxtempty.trim());
-  // console.log(eitems);
+  const EMAIL_TEXT = inputText // apple,banana , , orange ,
+    .split(",") // ['apple', 'banana ', '', ' orange'];
+    .filter((item) => item.trim() !== "") // ['apple', 'banana ', ' orange'];
+    .map((item) => item.trim()); // ['apple', 'banana', 'orange'];
+  // console.log(EMAIL_TEXT);
 
-  getemcontainer.innerHTML = "";
+  EMAIL_CONTAINER.innerHTML = "";
 
-  eitems.forEach(function (eitem) {
+  EMAIL_TEXT.forEach(function (eitem) {
     // console.log(eitem);
 
     let setnewspan = document.createElement("span");
     setnewspan.textContent = eitem;
     setnewspan.classList.add("email-item");
     // console.log(setnewspan);
-    getemcontainer.appendChild(setnewspan);
+    EMAIL_CONTAINER.appendChild(setnewspan);
   });
 }
 
-getbtn.addEventListener("click", function (e) {
+SEND_BTN.addEventListener("click", function (e) {
   e.preventDefault();
-  sendemail();
+  sendEmail();
 });
 
-function sendemail() {
-  const getaddresses = document.querySelectorAll(".email-item");
-  const gettxtvalue = gettextarea.value;
-  // console.log(getaddresses);
-  // console.log(gettxtvalue);
+function sendEmail() {
+  const ADDRESSES = document.querySelectorAll(".email-item");
+  const TEXT_VALUE = TEXTAREA.value;
+  // console.log(ADDRESSES);
+  // console.log(TEXT_VALUE);
 
-  var persons = [];
+  let persons = [];
 
-  if (getaddresses.length > 0 && gettxtvalue) {
-    getaddresses.forEach(function (getaddress) {
+  if (ADDRESSES.length > 0 && TEXT_VALUE) {
+    ADDRESSES.forEach(function (getaddress) {
       persons.push({
         email: getaddress.textContent,
-        message: gettxtvalue,
+        message: TEXT_VALUE,
       });
     });
     console.log(persons);
   } else {
     window.alert("Enter Message");
-    gettextarea.focus();
+    TEXTAREA.focus();
   }
 }
